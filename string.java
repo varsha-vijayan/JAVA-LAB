d74d5d0b36c080c46d5a96f04de415e2ed6614ec
varsha-vijayan <117884865+varsha-vijayan@users.noreply.github.com>
2023-03-29 20:34:33 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+ 
+
+public class GFG {
+    
+    public static String sortString(String inputString)
+    {
+        
+        char tempArray[] = inputString.toCharArray();
+ 
+        
+        Arrays.sort(tempArray);
+ 
+        
+        return new String(tempArray);
+    }
+ 
+   
+    public static void main(String[] args)
+    {
+        
+        String inputString = "geeksforgeeks";
+        String outputString = sortString(inputString);
+ 
+        
+        System.out.println("Input String : " + inputString);
+        
+        System.out.println("Output String : "
+                           + outputString);
+    }
+}
